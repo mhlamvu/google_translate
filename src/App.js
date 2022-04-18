49729caabc9e212caf255418d71aea76c39b0526
mhlamvu <bafana.mhlamvu@gmail.com>
2022-04-19 00:56:27 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,2 +1,9 @@
 
-export const App = () => <h1>Die!!!</h1>
+export const App = () => (
+  <>
+    <h1>Google Translate Clone</h1>
+    <input type="text" name="Language" />
+    <input type="text" name="Translate" />
+  </>
+
+  )
